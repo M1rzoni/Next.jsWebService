36da38ee2c98af564f70e1a3756f84ae21a7c349
Mirza <113027159+M1rzoni@users.noreply.github.com>
2025-11-28 15:29:20 +0100

    initial

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,67 +1,135 @@
-'use client'
+"use client";
 
-import { useState } from "react"
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function Home(){
+export default function Home() {
+  const router = useRouter();
 
-    const [file, setFile] = useState(null);
-    const [message, setMessage] = useState('');
-    const [isUploading, setIsUploading] = useState(false);
+  const [file, setFile] = useState(null);
+  const [duration, setDuration] = useState();
+  const [marketUnit, setMarketUnit] = useState();
+  const [message, setMessage] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
 
-    const handleFileChange = (event) =>{
-        setFile(event.target.files[0]);
-        setMessage('');
-    };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const loggedIn = localStorage.getItem("isLoggedIn");
+      if (!loggedIn) {
+        router.replace("/");
+      }
+    }
+  }, [router]);
 
-    const handleUpload = async () =>{
-         if(!file){
-            setMessage('Molimo dodajte file');
-            return;
-         }
-    
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+    setMessage("");
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    router.replace("/");
+  };
+
+  const handleDurationChange = (e) => {
+    setDuration(e.target.value);
+  };
+
+  const handleMarketUnit = (e) => {
+    setMarketUnit(e.target.value);
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      setMessage("Molimo dodajte file");
+      return;
+    }
+
+    if(!marketUnit){
+        setMessage('Molimo unesite poslovnu jedinicu')
+        return;
+    }
+
     setIsUploading(true);
 
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
+    formData.append("durationInSeconds", duration);
+    formData.append("marketUnit", marketUnit);
 
-    try{
-        const respones = await fetch("https://localhost:5000/upload", {
-            method: 'POST',
-            body: formData,
-        });
+    try {
+      const respones = await fetch("http://172.23.207.81:5000/upload", {
+        method: "POST",
+        body: formData,
+      });
 
-        if(respones.ok){
-            const data = await respones.json();
-            setMessage(`Upload upsješan! File name: ${data.fileName}` );
-            setFile(null);
-        }else{
-            setMessage('Upload failed')
-        }
+      if (respones.ok) {
+        const data = await respones.json();
+        setMessage(`Upload upsješan! File name: ${data.fileName}`);
+        setFile(null);
+        setDuration(null);
+        setMarketUnit(null);
+      } else {
+        setMessage("Upload failed");
+      }
+    } catch (error) {
+      console.log("Error file uploading:", error);
+      setMessage("An error occured during upload");
+    } finally {
+      setIsUploading(false);
+    }
+  };
 
-    }catch(error){
-        console.log("Error file uploading:", error)
-        setMessage('An error occured during upload');
+  return (
+    <div className="bg-gradient-to-r from-[#DA4453] to-[#89216B] h-screen w-screen flex items-center justify-center">
+      <div className="w-240 h-170 flex items-center justify-center bg-white rounded-xl relative">
+        <div className="flex flex-col items-center justify-center">
+          <div className="absolute top-0 items-center">
+            <h1 className="mt-10 text-xl">Upload Image&Video</h1>
+          </div>
+          <div className="absolute right-0 top-0 m-3">
+            <button className="bg-red-400 p-3" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+          <div>
+            <input
+              className="bg-[#e6e6e6] p-20 rounded-xl"
+              type="file"
+              onChange={handleFileChange}
+              accept="image/*,video/*"
+            />
+          </div>
+          <div>
+            <input
+              value={marketUnit}
+              onChange={handleMarketUnit}
+              type="number"
+              placeholder="Unesite poslovnu jedinicu"
+              className="mt-5 w-100 p-3 rounded-full text-sm"
+              required
+            />
+          </div>
 
-    }finally{
-        setIsUploading(false);
-    };
-};
-
-    return(
-       <div>
-        <h1>Upload Image&Video</h1>
-
-        <input type="file" onChange={handleFileChange} accept="image/*,video/*" />
-        <button onClick={handleUpload} disabled={!file || isUploading} 
-        >
-            {isUploading ? 'Uploading...' : 'Upload'}
-
-        </button>
-
-        {message && <p>{message}</p>}
-
-        
-       </div>
-    )
+          <div>
+            <input
+              value={duration || ""}
+              onChange={handleDurationChange}
+              type="number"
+              placeholder="Unesite dužinu trajanja slike (sekunde)"
+              className="mt-5 w-100 rounded-full p-3 text-sm"
+            />
+          </div>
+          <button
+            className="bg-[#57b846] p-5 mt-10 w-100 rounded-full"
+            onClick={handleUpload}
+            disabled={!file || isUploading}
+          >
+            {isUploading ? "Uploading..." : "Upload"}{" "}
+          </button>
+          <div className="text-red-400">{message + "!"}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,80 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import img from "./img.png"
-import {useState} from "react"
-import {useRouter} from "next/navigation"
+import Image from "next/image";
+import img from "./img.png";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { stringify } from "querystring";
+import Home from "./home/page";
 
 export default function Login() {
   const router = useRouter();
 
-  const HARDCODED_USERNAME = 'admin'
-  const HARDCODED_PASSWORD = 'admin'
+  const HARDCODED_USERNAME = "admin";
+  const HARDCODED_PASSWORD = "admin";
 
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
-  const [error, setError] = useState();
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState("");
 
-const handleLogin =(()=>{
+  useEffect(() => {
+    const isAuthenticated = localStorage.getItem("isLoggedIn") === "true";
 
-  event?.preventDefault();
+    if (isAuthenticated) {
+      router.replace("/home");
+    }
+  }, [router]);
 
-  if(username === HARDCODED_USERNAME && password === HARDCODED_PASSWORD){
-    router.push('/home')
-  }else{
-    setError(error)
-  }
-    })
-  return(
+  const handleLogin = (event) => {
+    event?.preventDefault();
+
+    if (username === HARDCODED_USERNAME && password === HARDCODED_PASSWORD) {
+      localStorage.setItem("isLoggedIn", "true");
+      router.push("/home");
+    } else {
+      setError("Pogrešan username ili password");
+    }
+  };
+  return (
     <div className="bg-gradient-to-r from-[#DA4453] to-[#89216B] h-screen w-screen flex items-center justify-center">
       <div className="w-240 h-170 flex items-center justify-center bg-white rounded-xl relative">
         <div className="flex items-center justify-center">
           <div className="flex flex-col  items-center justify-center">
-            <form className="flex flex-col absolute right-20 top-45" action="">
+            <form
+              className="flex flex-col absolute right-20 top-45"
+              action=""
+              onSubmit={handleLogin}
+            >
               <h1 className="text-2xl font-bold">Member Login</h1>
               <div className="mt-15 mb-4">
-            <input  value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" className="bg-[#e6e6e6] p-3 rounded-full w-70" type="username" required />
+                <input
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder="Username"
+                  className="bg-[#e6e6e6] p-3 rounded-full w-70"
+                  type="username"
+                  required
+                />
               </div>
-            <div>
-              <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" className="bg-[#e6e6e6] p-3 rounded-full w-70" type="password" />
-            </div>
-            <div>
-                <button type="submit" onClick={handleLogin} className="bg-[#57b846] mt-5 p-3 rounded-full text-white-500 w-70">Login</button>
-            </div>  
+              <div>
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  className="bg-[#e6e6e6] p-3 rounded-full w-70"
+                  type="password"
+                  required
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="bg-[#57b846] mt-5 p-3 rounded-full text-white-500 w-70"
+                >
+                  Login
+                </button>
+              </div>
+              {error && <p className="text-red-500 mt-2">{error}</p>}
             </form>
           </div>
         </div>
@@ -49,6 +83,5 @@ const handleLogin =(()=>{
         </div>
       </div>
     </div>
-  )
+  );
 }
-
